5a9812db6625c2fd3608495936a6ba1c9866750d
ADARSH PRABHAKAR <74524553+ADARSHPRABHAKAR002@users.noreply.github.com>
2024-06-10 23:01:45 +0530

    update3.0

--- build_project.sh
+++ build_project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Create a build directory
+rm -r build
 mkdir -p build
 cd build
 
@@ -14,15 +15,15 @@ make
 
 # Run the main executable
 #start=$(date +%s)
-/usr/bin/time -f "Total execution time %e seconds" ./main | tee ../BenchMarkScore/score.txt
+./main | tee ../BenchMarkScore/score.txt
 #end=$(date +%s)
 #echo "Elapsed time: $(($(($end-$start))/1000000)) ms"
-cd ..
-g++ -o main -I./include ./src/*.cpp -pg
-./main > output.txt 2>&1
-gprof main gmon.out > profiling_report.txt
+#cd ..
+#g++ -o main -I./include ./src/*.cpp -pg
+#./main > output.txt 2>&1
+#gprof main gmon.out > profiling_report.txt
 
-rm main output.txt gmon.out
+#rm main output.txt gmon.out
 
 # Clean up (optional)
-#make clean
+make clean
